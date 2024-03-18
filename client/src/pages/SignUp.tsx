291cd03ff73cb5acbac351b3d92075e7c023329f
ishmaifan <ishma07777@gmail.com>
2024-03-18 22:05:48 +0530

    Add alignment

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -211,12 +211,12 @@ function SignUp() {
                         <span className="w-full border-t" />
                       </div>
 
-                        <div className="relative flex justify-center text-xs uppercase">
-                          <span className="bg-background px-2 text-muted-foreground">
-                            Or continue with
-                          </span>
-                        </div>
+                      <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-background px-2 text-muted-foreground">
+                          Or continue with
+                        </span>
                       </div>
+
                       <div className="flex justify-center items-center ">
                         {/*google AUTH*/}
                         <GoogleAuth
